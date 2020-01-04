cd743920258631e4b9ac88ba23be5cb8597bdb62
Nick Rirush <RaccoonRirush@gmail.com>
2020-01-04 20:29:23 +0800

    Code cleanup

--- cmd/certificateIssuer/main.go
+++ cmd/certificateIssuer/main.go
@@ -18,6 +18,85 @@ const Usage = `Usage:
                       verifyCertificate [pubkey] [certificate] [signature]
                       renewCertificate [privkey] [expiredCertificate] [newCertificate] [validUntil]`
 
+func WriteKeysToFile(pubkey, privkey []byte, pubkeyPath, privkeyPath string) {
+	pubkey, privkey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile(pubkeyPath, pubkey, 0700)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile(privkeyPath, privkey, 0700)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+func FormCertificate(validUntil, description, serverUUID string) (protocol.ServerCertificate, ed25519.PrivateKey) {
+	t, err := tparse.ParseNow(time.ANSIC, validUntil)
+	if err != nil {
+		fmt.Println("Cannot parse time:", err)
+		os.Exit(1)
+	}
+	marshalledTime, err := t.MarshalText()
+	if err != nil {
+		fmt.Println("Cannot marshal time:", err)
+		os.Exit(1)
+	}
+	validUntil = string(marshalledTime)
+
+	if serverUUID == "" {
+		u, err := uuid.NewRandom()
+		if err != nil {
+			fmt.Println("Cannot generate UUID:", err)
+			os.Exit(1)
+		}
+		serverUUID = u.String()
+	} else {
+		u, err := uuid.Parse(serverUUID)
+		if err != nil {
+			fmt.Println("Invalid UUID:", err)
+			os.Exit(1)
+		}
+		serverUUID = u.String()
+	}
+
+	serverPubKey, serverPrivKey, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		fmt.Println("Cannot generate server key:", err)
+		os.Exit(1)
+	}
+
+	return protocol.ServerCertificate{
+		ServerID:    serverUUID,
+		Expiration:  validUntil,
+		SigningKey:  serverPubKey,
+		Description: description,
+	}, serverPrivKey
+}
+
+func WriteCertificateFiles(certificate, signature, privKey []byte, basePath string) {
+	err := ioutil.WriteFile(basePath, certificate, 0700)
+	if err != nil {
+		fmt.Println("Cannot write certificate:", err)
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile(basePath+".priv", privKey, 0700)
+	if err != nil {
+		fmt.Println("Cannot marshal certificate:", err)
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile(basePath+".sig", signature, 0700)
+	if err != nil {
+		fmt.Println("Cannot marshal certificate:", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println(Usage)
@@ -29,23 +108,12 @@ func main() {
 			fmt.Println(Usage)
 			os.Exit(1)
 		}
-		pubKeyPath := os.Args[2]
-		privKeyPath := os.Args[3]
 		pubkey, privkey, err := ed25519.GenerateKey(nil)
 		if err != nil {
-			fmt.Println("Error:", err)
+			fmt.Println("Cannot create key pair:", err)
 			os.Exit(1)
 		}
-		err = ioutil.WriteFile(pubKeyPath, pubkey, 0755)
-		if err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-		err = ioutil.WriteFile(privKeyPath, privkey, 0755)
-		if err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
+		WriteKeysToFile(pubkey, privkey, os.Args[2], os.Args[3])
 	case "issueCertificate":
 		if len(os.Args) != 6 && len(os.Args) != 7 {
 			fmt.Println(Usage)
@@ -62,114 +130,66 @@ func main() {
 			os.Exit(1)
 		}
 		pKey := ed25519.PrivateKey(key)
-		certPath := os.Args[3]
-		validUntil := os.Args[4]
-		t, err := tparse.ParseNow(time.ANSIC, validUntil)
-		if err != nil {
-			fmt.Println("Cannot parse time:", err)
-			os.Exit(1)
+		serverUUID := ""
+		if len(os.Args) == 7 {
+			serverUUID = os.Args[6]
 		}
-		marshalledTime, err := t.MarshalText()
-		if err != nil {
-			fmt.Println("Cannot marshal time:", err)
-			os.Exit(1)
-		}
-		validUntil = string(marshalledTime)
-		description := os.Args[5]
-		var serverUuid string
-		if len(os.Args) == 6 {
-			u, err := uuid.NewRandom()
-			if err != nil {
-				fmt.Println("Cannot generate UUID:", err)
-				os.Exit(1)
-			}
-			serverUuid = u.String()
-		} else {
-			u, err := uuid.Parse(os.Args[6])
-			if err != nil {
-				fmt.Println("Invalid UUID:", err)
-				os.Exit(1)
-			}
-			serverUuid = u.String()
-		}
-		serverPubKey, serverPrivKey, err := ed25519.GenerateKey(nil)
-		if err != nil {
-			fmt.Println("Cannot generate server key:", err)
-			os.Exit(1)
-		}
-		certificate := protocol.ServerCertificate{
-			ServerID: serverUuid,
-			Expiration: validUntil,
-			Description: description,
-			SigningKey: serverPubKey,
-		}
+
+		certificate, serverPrivKey := FormCertificate(os.Args[4], os.Args[5], serverUUID)
 		marshalledCertificate, err := json.Marshal(certificate)
 		if err != nil {
 			fmt.Println("Cannot marshal certificate:", err)
 			os.Exit(1)
 		}
 		signature := ed25519.Sign(pKey, marshalledCertificate)
-		err = ioutil.WriteFile(certPath, marshalledCertificate, 0700)
-		if err != nil {
-			fmt.Println("Cannot write certificate:", err)
-			os.Exit(1)
-		}
-		err = ioutil.WriteFile(certPath + ".priv", serverPrivKey, 0700)
-		if err != nil {
-			fmt.Println("Cannot marshal certificate:", err)
-			os.Exit(1)
-		}
-		err = ioutil.WriteFile(certPath + ".sig", signature, 0700)
-		if err != nil {
-			fmt.Println("Cannot marshal certificate:", err)
-			os.Exit(1)
-		}
+
+		WriteCertificateFiles(marshalledCertificate, signature, serverPrivKey, os.Args[3])
 
 	case "verifyCertificate":
 		if len(os.Args) != 5 {
 			fmt.Println(Usage)
 			os.Exit(1)
 		}
-		pubKeyPath := os.Args[2]
-		certPath := os.Args[3]
-		signaturePath := os.Args[4]
-		pubkeyBytes, err := ioutil.ReadFile(pubKeyPath)
+
+		pubkeyBytes, err := ioutil.ReadFile(os.Args[2])
 		if err != nil {
 			fmt.Println("Cannot read public key:", err)
 			os.Exit(1)
 		}
 		pubkey := ed25519.PublicKey(pubkeyBytes)
-		signature, err := ioutil.ReadFile(signaturePath)
+		signature, err := ioutil.ReadFile(os.Args[4])
 		if err != nil {
 			fmt.Println("Cannot read signature:", err)
 			os.Exit(1)
 		}
-		certificate, err := ioutil.ReadFile(certPath)
+		certificate, err := ioutil.ReadFile(os.Args[3])
 		if err != nil {
 			fmt.Println("Cannot read certificate:", err)
 			os.Exit(1)
 		}
+
 		if ed25519.Verify(pubkey, certificate, signature) {
 			cert := protocol.ServerCertificate{}
 			json.Unmarshal(certificate, &cert)
 			expirationDate, _ := time.Parse(time.RFC3339, cert.Expiration)
 			if expirationDate.Before(time.Now()) {
 				fmt.Println("Certificate is expired")
+				os.Exit(1)
 			} else {
 				fmt.Println("Certificate is valid")
 			}
 		} else {
 			fmt.Println("Certificate is invalid")
+			os.Exit(2)
 		}
-
 
 	case "renewCertificate":
 		if len(os.Args) != 6 {
 			fmt.Println(Usage)
 			os.Exit(1)
 		}
-		privKeyPath := os.Args[2]
-		privKeyBytes, err := ioutil.ReadFile(privKeyPath)
+
+		privKeyBytes, err := ioutil.ReadFile(os.Args[2])
 		if err != nil {
 			fmt.Println("Cannot read private key:", err)
 			os.Exit(1)
@@ -179,67 +199,27 @@ func main() {
 			os.Exit(1)
 		}
 		privKey := ed25519.PrivateKey(privKeyBytes)
-		expiredCertPath := os.Args[3]
-		expiredCertBytes, err := ioutil.ReadFile(expiredCertPath)
+		expiredCertBytes, err := ioutil.ReadFile(os.Args[3])
 		if err != nil {
 			fmt.Println("Cannot read expired certificate:", err)
 			os.Exit(1)
 		}
+
 		expiredCert := protocol.ServerCertificate{}
 		err = json.Unmarshal(expiredCertBytes, &expiredCert)
 		if err != nil {
 			fmt.Println("Cannot unmarshal certificate:", err)
 			os.Exit(1)
 		}
-		newCertPath := os.Args[4]
-		validUntil := os.Args[5]
-		t, err := tparse.ParseNow(time.ANSIC, validUntil)
-		if err != nil {
-			fmt.Println("Cannot parse time:", err)
-			os.Exit(1)
-		}
-		marshalledTime, err := t.MarshalText()
-		if err != nil {
-			fmt.Println("Cannot marshal time:", err)
-			os.Exit(1)
-		}
-		serverPubKey, serverPrivKey, err := ed25519.GenerateKey(nil)
-		if err != nil {
-			fmt.Println("Cannot generate server key:", err)
-			os.Exit(1)
-		}
-		u, err := uuid.Parse(expiredCert.ServerID)
-		if err != nil {
-			fmt.Println("Invalid UUID:", err)
-			os.Exit(1)
-		}
-		expiredCert.ServerID = u.String()
-		newCertificate := protocol.ServerCertificate{
-			ServerID:    expiredCert.ServerID,
-			Expiration:  string(marshalledTime),
-			SigningKey:  serverPubKey,
-			Description: expiredCert.Description,
-		}
-		marshalledCertificate, err := json.Marshal(newCertificate)
+
+		certificate, serverPrivKey := FormCertificate(os.Args[5], expiredCert.Description, expiredCert.ServerID)
+		marshalledCertificate, err := json.Marshal(certificate)
 		if err != nil {
 			fmt.Println("Cannot marshal certificate:", err)
 			os.Exit(1)
 		}
 		signature := ed25519.Sign(privKey, marshalledCertificate)
-		err = ioutil.WriteFile(newCertPath, marshalledCertificate, 0700)
-		if err != nil {
-			fmt.Println("Cannot write certificate:", err)
-			os.Exit(1)
-		}
-		err = ioutil.WriteFile(newCertPath + ".priv", serverPrivKey, 0700)
-		if err != nil {
-			fmt.Println("Cannot marshal certificate:", err)
-			os.Exit(1)
-		}
-		err = ioutil.WriteFile(newCertPath + ".sig", signature, 0700)
-		if err != nil {
-			fmt.Println("Cannot marshal certificate:", err)
-			os.Exit(1)
-		}
+
+		WriteCertificateFiles(marshalledCertificate, signature, serverPrivKey, os.Args[4])
 	}
 }
